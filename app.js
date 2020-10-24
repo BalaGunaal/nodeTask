f9543c9d3f8671395e68a913c461259b5c6aac97
balaGunaal <59305036+BalaGunaal@users.noreply.github.com>
2020-10-24 15:46:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ let port = process.env.PORT;
 if (port == null || port == "") {
   port = 5000;
 }
-app.listen(port);
+
 
 app.listen(port, function() {
   console.log("Server started on port 3000");
